339c6106f46377795f1c63372b48021af8efa765
Y-Kilic <CyanGithub@protonmail.com>
2025-07-15 22:24:32 +0200

    Refine overseer prompts and add data

---
+++ scripts/fetch_trials.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def load_trials():
+    """Return a DataFrame of supplement trial results."""
+    # TODO: replace with real data fetching logic
+    return pd.read_csv('/data/supplements.csv')
+
